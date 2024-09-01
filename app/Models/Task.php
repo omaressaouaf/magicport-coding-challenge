5a6ede9943bfba87f5172e2d659b01a515383754
omaressaouaf <souafomar@gmail.com>
2024-09-01 17:30:40 +0100

    setup migrations and models for Project, Task

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected array $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class
+        ];
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
