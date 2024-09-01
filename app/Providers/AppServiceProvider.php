9f6da39cf9c47cc240e2cacd1ca5811af615afea
omaressaouaf <souafomar@gmail.com>
2024-09-01 18:09:25 +0100

    hook permissions into gate system

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
+use App\Models\User;
+use App\Services\PermissionService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $permissionsNames = Cache::rememberForever('permissions', fn() => Permission::all()->pluck('name'));
+
+        foreach ($permissionsNames as $permissionName) {
+            Gate::define($permissionName, function (User $user) use ($permissionName) {
+                /**
+                 * @var PermissionService $permissionService
+                 */
+                $permissionService = $this->app->make(PermissionService::class);
+
+                return $permissionService->userHasPermission($user, $permissionName);
+            });
+        }
     }
 }
